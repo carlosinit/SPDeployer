e67335fe72c8e53aaa90cf4bfedf8fc8325c75c7
Carlos Filipe da Silva Oliveira <dasilva.carlos@outlook.com>
2016-01-14 23:10:11 +0100

    Init

---
+++ SPDeployer/Program.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+
+namespace SPDeployer
+{
+    class Program
+    {
+        static List<IDisposable> streams = new List<IDisposable>();
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                if (args.Length != 5)
+                {
+                    throw new InvalidOperationException(
+                        @"You must pass 5 parameters in the following order [webUrl] [user] [password] [folder] [sourceFolderPath]. 
+                        Example: 
+                        SPDeployer.exe http://mysupersite.sharepoint.com/ user@domain.com P@55w0rd SiteAssets/SomePlace C:\temp\files");
+                }
+
+                var webSiteUrl = ParseWebUrl(args[0]);
+                var user = ParseUser(args[1]);
+                var securedPassword = ParsePassword(args[2]);
+                var applicationFolderPath = ParseApplicationFolderPath(args[3]);
+                var sourceFolderPath = ParseSourceFolderPath(args[4]);
+
+                using (var clientContext = new ClientContext(webSiteUrl))
+                {
+                    clientContext.Credentials = new SharePointOnlineCredentials(user, securedPassword);
+
+                    var web = clientContext.Web;
+                    var applicationFolder = web.GetFolderByServerRelativeUrl(applicationFolderPath);
+
+                    clientContext.Load(web);
+                    clientContext.Load(applicationFolder);
+
+                    try
+                    {
+                        clientContext.ExecuteQuery();
+                    }
+                    catch (ServerException exception)
+                    {
+                        if (exception.ServerErrorTypeName == "System.IO.FileNotFoundException")
+                        {
+                            applicationFolder = web.Folders.Add(applicationFolderPath);
+                            Console.WriteLine("Could not find application folder, creating it...");
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+
+                    CreateFolderStructure(new DirectoryInfo(sourceFolderPath), applicationFolder);
+                    Console.WriteLine("Sending the files to SharePoint...");
+                    clientContext.ExecuteQuery();
+
+                }
+
+                stopwatch.Stop();
+                Console.WriteLine($"Operation completed after {stopwatch.Elapsed.TotalSeconds}s");
+                return 0;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{exception.GetType()}: {exception.Message}");
+                return -1;
+            }
+            finally
+            {
+                foreach (var item in streams)
+                {
+                    item.Dispose();
+                }
+            }
+        }
+
+        static string ParseSourceFolderPath(string sourceFolderPathString)
+        {
+            if (string.IsNullOrWhiteSpace(sourceFolderPathString))
+            {
+                throw new ArgumentException("Invalid source folder path");
+            }
+
+            if (!Directory.Exists(sourceFolderPathString))
+            {
+                throw new ArgumentException("Source folder path does not exist");
+            }
+
+            return sourceFolderPathString;
+        }
+
+        static string ParseApplicationFolderPath(string applicationFolderPath)
+        {
+            if (string.IsNullOrWhiteSpace(applicationFolderPath))
+            {
+                throw new ArgumentException("Invalid application folder name");
+            }
+
+            if (applicationFolderPath.StartsWith("/", StringComparison.Ordinal))
+            {
+                applicationFolderPath = applicationFolderPath.Substring(1);
+            }
+            return applicationFolderPath;
+        }
+
+        static SecureString ParsePassword(string passwordString)
+        {
+            var securedPassword = new SecureString();
+            foreach (var c in passwordString)
+            {
+                securedPassword.AppendChar(c);
+            }
+            return securedPassword;
+
+        }
+
+        static string ParseUser(string userString)
+        {
+            if (!userString.Contains("@"))
+            {
+                throw new ArgumentException("Invalid user name");
+            }
+
+            return userString;
+        }
+
+        static string ParseWebUrl(string urlString)
+        {
+            if (!urlString.ToLowerInvariant().StartsWith("https://", StringComparison.Ordinal))
+            {
+                throw new ArgumentException("Invalid web url");
+            }
+            return urlString;
+        }
+
+        static void CreateFolderStructure(DirectoryInfo input, Folder output)
+        {
+            foreach (var item in input.GetFileSystemInfos())
+            {
+                if (item is DirectoryInfo)
+                {
+                    CreateFolderStructure(item as DirectoryInfo, output.Folders.Add(item.Name));
+                }
+                else
+                {
+                    var itemStream = (item as FileInfo).OpenRead();
+                    streams.Add(itemStream);
+
+                    var creationInfo = new FileCreationInformation();
+                    creationInfo.Overwrite = true;
+                    creationInfo.Url = item.Name;
+                    creationInfo.ContentStream = itemStream;
+                    output.Files.Add(creationInfo);
+
+
+                }
+            }
+        }
+    }
+}
---
+++ SPDeployer/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SPDeployer")]
+[assembly: AssemblyDescription("Deploys files to SharePoint Online")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("carlosinit")]
+[assembly: AssemblyProduct("SPDeployer")]
+[assembly: AssemblyCopyright("Copyright © carlosinit 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fd8ada0-3570-4499-b5c4-318435660ece")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
